b2b9a9d24a890f8eb6829d4f83029cf703399ca3
Yarden Naor <naoryarden@gmail.com>
2022-12-14 11:02:22 +0200

    finished createImgs

--- js/meme.service.js
+++ js/meme.service.js
@@ -1,9 +1,11 @@
 'use strict'
 
-var gImgs
+var gImgs=[]
 var nextIdx=1
 
  console.log('hi from sevice:')
+
+ _createImgs()
 
 function getImgs(){
 return gImgs
@@ -12,13 +14,17 @@ return gImgs
 
 function _createImgs(){
 
-while (`img/${nextIdx}.jpg`){
+// while (`img/${nextIdx}.jpg`){
+for (var i=0;i<18;i++){
  const img= {
 id: nextIdx++, 
-url: `img/${nextIdx}.jpg`, 
+url: `img/${i+1}.jpg`, 
 keywords: []
+//  }
  }
-}
  gImgs.push(img)
+}
+
+console.log('imgs:',gImgs)
 }
 
