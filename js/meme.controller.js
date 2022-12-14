e40da1b9d5e5e8f5e3687c9500aff1c19f9c913b
Yarden Naor <naoryarden@gmail.com>
2022-12-14 11:34:55 +0200

    finished rendering gallery

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -3,13 +3,24 @@
 
 console.log('hi from controller:')
 
-function onInit(){
+function onInit() {
     renderGallery()
 }
 
-function renderGallery(){
-const gallery= getImgs()
+function renderGallery() {
+    const imgs = getImgs()
+    console.log('imgs at render:', imgs)
 
+    const elGallery = document.querySelector('.gallery-container')
+
+    // console.log('elgallery:',elGallery)
+    const strHTMLs = imgs.map(img => {
+        return `<article>
+<img data-id="${img.id}" src="${img.url}"  >
+</article>`
+    })
+    console.log('strHTMLs:', strHTMLs)
+    elGallery.innerHTML = strHTMLs.join('')
 
 
 }
